9bd3ae1729a3492f99d1d451ef29730a5cd27f93
QuanLi <quanlidavid@gmail.com>
2022-02-27 18:24:02 +0800

    JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,6 +2,7 @@
 
 console.log('App,js is running'); //JSX - JavaScript XML
 
-var template = /*#__PURE__*/React.createElement("h1", null, "Indecision App");
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecision App"), /*#__PURE__*/React.createElement("p", null, "This is some info"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
+var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "David"), /*#__PURE__*/React.createElement("p", null, "Age: 20"), /*#__PURE__*/React.createElement("p", null, "Location: Japan"));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,24 @@
 console.log('App,js is running');
 
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
+var template = (
+	<div>
+		<h1>Indecision App</h1>
+		<p>This is some info</p>
+		<ol>
+			<li>Item one</li>
+			<li>Item two</li>
+		</ol>
+	</div>
+);
+
+var template2 = (
+	<div>
+		<h1>David</h1>
+		<p>Age: 20</p>
+		<p>Location: Japan</p>
+	</div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
